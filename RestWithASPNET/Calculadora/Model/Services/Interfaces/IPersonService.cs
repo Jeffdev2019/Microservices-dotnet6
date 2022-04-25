8c723aaaa3bdcc83e62b8fde021ceadd89c0f514
Jefferson <jeffersonvic1@gmail.com>
2022-04-25 04:28:17 -0300

    API Pessoa concluida

---
+++ RestWithASPNET/Calculadora/Model/Services/Interfaces/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNET.Model.Services.Interfaces
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
--- RestWithASPNET/Calculadora/Program.cs
+++ RestWithASPNET/Calculadora/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using RestWithASPNET.Model.Services.Implementations;
+using RestWithASPNET.Model.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 
 var app = builder.Build();
 
